2a53dcb144ae997ab743a5fe801b16fe39e81a2a
Faris Hasim <farishasimsyauqi@gmail.com>
2021-04-04 10:07:55 +0700

    update a* + add test case

--- map.py
+++ map.py
@@ -28,3 +28,29 @@ class Map(graph.Graph):
 				return None
 			return cur
 		return []
+
+	# alternatif lain tanpa dfs
+	def a_star_path2(this,origin,target):
+		# if (this.dfs_search(origin.get_name(),target.get_name())):
+		visited_dict = {}
+		for nodes in this.nodes:
+			visited_dict[nodes.get_name()] = False
+		queue = pq.PrioQueueMap()
+		initial_element = pq.ElementQueueMap([origin],target)
+		queue.enqueue(initial_element)
+		cur = initial_element.track
+		while(cur[len(cur)-1].get_name() != target.get_name() and len(queue.queue) != 0):
+			cur = queue.dequeue().track #bentuknya list node
+			visited_dict.update({cur[len(cur)-1].get_name() : True}) # tandai sudah dikunjungi
+			neighbor = cur[len(cur)-1].get_all_neighbor()
+			new_track = cur[:]
+			for it in neighbor:
+				if not visited_dict.get(it.get_name()):
+					new_track.append(it)
+					new_elqueue = pq.ElementQueueMap(new_track,target)
+					queue.enqueue(new_elqueue)
+					new_track = cur[:]
+		if (len(queue.queue) == 0):
+			return []
+		return cur
+		# return []
--- testsc.py
+++ testsc.py
@@ -6,11 +6,11 @@ import mapVisualizer as mv
 
 
 
-peta = ml.MapLoader().load("test/testITB.txt")
+peta = ml.MapLoader().load("test/testITB2.txt")
 # peta.print_all()
 
 # result = peta.a_star_path(a,h)
-result = peta.a_star_path(peta.get_node("A"),peta.get_node("H"))
+result = peta.a_star_path2(peta.get_node("A"),peta.get_node("H"))
 # print(result)
 if (len(result) != 0):
     for i in result:
