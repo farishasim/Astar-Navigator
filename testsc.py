94135ecd0caac0ae2e74997494017487ca0fbc5f
Randy Zakya <impper1@gmail.com>
2021-04-06 07:59:14 +0700

    add cli output

--- testsc.py
+++ testsc.py
@@ -17,9 +17,15 @@ node2 = input()
 result = peta.a_star_path2(peta.get_node(node1),peta.get_node(node2))
 # print(result)
 if (len(result[0]) != 0):
-    for i in result[0]:
-        print(i.get_name())
-    print("%.2f" % result[1])
+    for i in range(len(result[0])):
+        print(result[0][i].get_name() , end = "")
+        if (i != len(result[0]) -1):
+            print(" --> " , end = "")
+        else:
+            print()
+    # for i in result[0]:
+    #     print(i.get_name() , end = "")
+    print("%.2f" % result[1] + " metres")
     mv.MapVisualizer().visualize(peta, result[0])
     url = 'file:///' + os.getcwd() + '/' + 'testscmap.html'
     webbrowser.open(url) 
